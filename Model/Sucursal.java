58d07f4f18226d40341b8422e363b26a2a118150
mayron73 <107969219+mayron73@users.noreply.github.com>
2022-07-25 20:37:19 -0500

    Add files via upload

---
+++ Model/Sucursal.java
@@ -0,0 +1,39 @@
+
+package Model;
+
+
+public class Sucursal {
+    private int idSucursal;
+    private  String sucursalName;
+
+    public Sucursal() {
+        idSucursal = 0;
+        sucursalName = "";
+    }
+
+    public int getIdSucursal() {
+        return idSucursal;
+    }
+
+    public void setIdSucursal(int idSucursal) {
+        this.idSucursal = idSucursal;
+    }
+
+    public String getSucursalName() {
+        return sucursalName;
+    }
+
+    public void setSucursalName(String sucursalName) {
+        this.sucursalName = sucursalName;
+    }
+
+    @Override
+    public String toString() {
+        return getSucursalName();
+    }
+    
+    
+    
+    
+}
+
